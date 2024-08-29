74b8577b545552e11ca7855d6562c8f9958bf785
marcopeg <marco.pegoraro@gmail.com>
2024-08-29 09:28:25 +0200

    Improve home

--- app/src/AppDesktop/screens/HomeScreen/HomeScreen.tsx
+++ app/src/AppDesktop/screens/HomeScreen/HomeScreen.tsx
@@ -1,3 +1,32 @@
-export const HomeScreen = () => {
-  return "HomeScreen";
+import React from "react";
+import { Container, Box, Typography } from "@mui/material";
+
+export const HomeScreen: React.FC = () => {
+  return (
+    <Container
+      maxWidth="sm"
+      sx={{
+        height: "30vh",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <Box>
+        <Typography
+          variant="h2"
+          align="center"
+          sx={{ fontWeight: "bold", color: "primary.main" }}
+        >
+          Welcome to TOOLS
+        </Typography>
+        <Typography variant="body1" align="center" mt={2}>
+          Here you can find a collection of tools to help you with your daily
+          struggle as a developer.
+        </Typography>
+      </Box>
+    </Container>
+  );
 };
+
+export default HomeScreen;
